4f9c2eb7f1c15e67596eb02787b9226b136e8d5a
Tirq <tiagogelatti@outlook.com>
2018-04-18 21:08:04 -0300

    Initial commit

---
+++ src/main/java/com/tirq/java_eight_samples/App.java
@@ -0,0 +1,13 @@
+package com.tirq.java_eight_samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from java 8!" );
+    }
+}
